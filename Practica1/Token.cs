3099e8d29b8f64a4eecb257846ece3b013588c73
LumationDevs <Eguix-2010302@kinal.edu.gt>
2019-02-10 20:33:54 -0600

    Agregando tokens

--- Practica1/Token.cs
+++ Practica1/Token.cs
@@ -8,6 +8,29 @@ namespace Practica1
 {
     class Token
     {
+        int no;
+        int tokens;
+        string lexema;
+        string tipo;
+        int fila;
+        int columna;
 
+        public int No { get => no; set => no = value; }
+        public int Tokens { get => tokens; set => tokens = value; }
+        public string Lexema { get => lexema; set => lexema = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public int Fila { get => fila; set => fila = value; }
+        public int Columna { get => columna; set => columna = value; }
+
+        public Token() {
+        }
+        public Token(int no, int tokens,string lexema, string tipo,int fila,int columna) {
+            this.no = no;
+            this.tokens = tokens;
+            this.lexema = lexema;
+            this.tipo = tipo;
+            this.fila = fila;
+            this.columna = columna;
+        }
     }
 }
